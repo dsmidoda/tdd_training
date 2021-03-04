be213f9aef4f966bb8dcb4a96153ad15bced36b8
DSMIDODA <dawidsmidoda027@gmail.com>
2021-03-04 09:33:10 +0100

    Making First Pull Request

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative tasks. Test pull request...."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superlists.settings')
     try:
         from django.core.management import execute_from_command_line
